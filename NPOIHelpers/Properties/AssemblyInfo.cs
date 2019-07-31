2df9091d7d2d463bfa99974047208d5d9c1f1048
snlh@simcorp.com <snlh@simcorp.com>
2019-07-31 10:16:03 +0200

    A bit of renaming

--- NPOIHelpers/Properties/AssemblyInfo.cs
+++ NPOIHelpers/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NPOIHelpers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Array based interface to NPOI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NPOIHelpers")]
+[assembly: AssemblyCompany("SimCorp Italiana")]
+[assembly: AssemblyProduct("SimCorp Sofia")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
